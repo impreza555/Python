26526a396d81f873f4c9c7460f0bd636a60cec6f
impreza555 <alex.erjomin@outlook.com>
2021-02-03 22:34:35 +0300

    Улучшил код упражнения 2.3

--- exercise_2.3.py
+++ exercise_2.3.py
@@ -27,15 +27,15 @@ if month < 1 or month > 12:
 elif month == 12:
     print(
         f'Месяц {months_dict[list(months_dict.keys())[0]][0]} относится ко времени года {list(months_dict.keys())[0]}')
-elif month == 1 or month == 2:
+elif month in (1, 2):
     print(
         f'Месяц {months_dict[list(months_dict.keys())[0]][month]} относится ко времени года {list(months_dict.keys())[0]}')
-elif month == 3 or month == 4 or month == 5:
+elif month in (3, 4, 5):
     print(
         f'Месяц {months_dict[list(months_dict.keys())[1]][month - 3]} относится ко времени года {list(months_dict.keys())[1]}')
-elif month == 6 or month == 7 or month == 8:
+elif month in (6, 7, 8):
     print(
         f'Месяц {months_dict[list(months_dict.keys())[2]][month - 6]} относится ко времени года {list(months_dict.keys())[2]}')
-elif month == 9 or month == 10 or month == 11:
+elif month in (9, 10, 11):
     print(
         f'Месяц {months_dict[list(months_dict.keys())[3]][month - 9]} относится ко времени года {list(months_dict.keys())[3]}')
