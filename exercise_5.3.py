3540076b6650c85844b2134022eafe6253be3db4
impreza555 <alex.erjomin@outlook.com>
2021-02-14 22:46:56 +0300

    Сделал упражнение 5.3.

--- exercise_5.3.py
+++ exercise_5.3.py
@@ -1,5 +1,20 @@
-"""
-3. Создать текстовый файл (не программно), построчно записать фамилии сотрудников и величину их окладов.
-Определить, кто из сотрудников имеет оклад менее 20 тыс., вывести фамилии этих сотрудников.
-Выполнить подсчет средней величины дохода сотрудников.
-"""
+try:
+    with open('text_3.txt', 'r', encoding='utf-8') as my_f:
+        empl_dict = {}
+        for line in my_f:
+            print(line, end='')
+            line_list = line.split()
+            empl_dict.setdefault(line_list[0], float(line_list[1]))
+except IOError:
+    print('Ошибка ввода-вывода!')
+finally:
+    my_f.close()
+required_salary = 20000
+losers = []
+salaries = []
+for name, salary in empl_dict.items():
+    salaries.append(salary)
+    if salary < required_salary:
+        losers.append(name)
+print(f'\n{"-" * 82}\nСотрудники: {(", ".join(name for name in list(losers)))} имеют оклад менее {required_salary}!')
+print(f'{"-" * 82}\nСредняя величина дохода сотрудников {sum(salaries) / len(salaries)}')
