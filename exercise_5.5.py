e0bcaedd91af2b9b2c3a7ddea723fd0f28f0dab4
impreza555 <alex.erjomin@outlook.com>
2021-02-14 18:58:09 +0300

    Сделал упражнение 5.5.

--- exercise_5.5.py
+++ exercise_5.5.py
@@ -1,4 +1,12 @@
-"""
-5. Создать (программно) текстовый файл, записать в него программно набор чисел, разделенных пробелами.
-Программа должна подсчитывать сумму чисел в файле и выводить ее на экран.
-"""
+some_file = open('exercise_5.5.txt', 'w', encoding='utf-8')
+some_text = input('Введите несколько чисел через пробелы: ')
+some_file.write(some_text)
+some_file.close()
+some_file = open('exercise_5.5.txt', 'r', encoding='utf-8')
+content = some_file.read()
+some_file.close()
+content = content.split()
+result_content = []
+for i in content:
+    if i.lstrip('-').replace('.', '', 1).isdigit(): result_content.append(i)
+print(sum(map(float, result_content)))
