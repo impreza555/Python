3178bdbdba96ab07a0de0a8ce1f098e2d8b5ba99
impreza555 <alex.erjomin@outlook.com>
2021-02-14 16:41:16 +0300

    Сделал упражнение 5.4.

--- exercise_5.1.py
+++ exercise_5.1.py
@@ -1,4 +1,4 @@
-some_file = open('exercise_5.1.txt', 'w', encoding="utf-8")
+some_file = open('exercise_5.1.txt', 'w', encoding='utf-8')
 some_text = input('Введите какой-нибудь текст: ')
 while some_text:
     some_file.write(some_text + '\n')
--- exercise_5.4.py
+++ exercise_5.4.py
@@ -1,10 +1,14 @@
-"""
-4. Создать (не программно) текстовый файл со следующим содержимым:
-One — 1
-Two — 2
-Three — 3
-Four — 4
-Необходимо написать программу, открывающую файл на чтение и считывающую построчно данные.
-При этом английские числительные должны заменяться на русские.
-Новый блок строк должен записываться в новый текстовый файл.
-"""
+from googletrans import Translator
+
+# Всю голову изломал с этим заданием. Всё время выходила ошибка,
+# причём не в моём коде, а где-то в модуле googletrans.
+# Погугил, и нашёл решение: надо усанавливать альфа-версию googletrans
+# pip install googletrans==4.0.0-rc1
+
+translator = Translator()
+
+my_f = open('text_4.txt', 'r', encoding='utf-8')
+contents = my_f.read()
+result = translator.translate(contents, dest='ru')
+with open('text_translate_4.txt', 'w', encoding='utf-8') as my_f:
+    my_f.write(result.text)
