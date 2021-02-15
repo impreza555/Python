66270e4283a2fc85bd15c8ece780d26987ced970
impreza555 <alex.erjomin@outlook.com>
2021-02-15 22:47:57 +0300

    Сделал упражнение 5.7

--- exercise_5.7.py
+++ exercise_5.7.py
@@ -1,15 +1,31 @@
-"""
-7. Создать (не программно) текстовый файл, в котором каждая строка должна содержать данные о фирме:
-название, форма собственности, выручка, издержки.
-Пример строки файла: firm_1 ООО 10000 5000.
-Необходимо построчно прочитать файл, вычислить прибыль каждой компании, а также среднюю прибыль.
-Если фирма получила убытки, в расчет средней прибыли ее не включать.
-Далее реализовать список. Он должен содержать словарь с фирмами и их прибылями, а также словарь со средней прибылью.
-Если фирма получила убытки, также добавить ее в словарь (со значением убытков).
-Пример списка: [{“firm_1”: 5000, “firm_2”: 3000, “firm_3”: 1000}, {“average_profit”: 2000}].
-Итоговый список сохранить в виде json-объекта в соответствующий файл.
-Пример json-объекта:
-[{"firm_1": 5000, "firm_2": 3000, "firm_3": 1000}, {"average_profit": 2000}]
+from json import dumps
 
-Подсказка: использовать менеджеры контекста.
-"""
+list_of_firms = []
+profit = {}
+total_profit = 0
+i = 0
+try:
+    with open('text_7.txt', 'r', encoding='utf-8') as my_f:
+        firm_content = my_f.read()
+        my_f.seek(0)
+        print(f'Фирмы и их выручка / издержки:\n{"-" * 30}\n{firm_content}')
+        for line in my_f:
+            name, type_of_ownership, earnings, expenses = line.split()
+            profit[name] = float(earnings) - float(expenses)
+            if profit.setdefault(name) >= 0:
+                total_profit = total_profit + profit.setdefault(name)
+                i += 1
+        if i != 0:
+            average_profit = round((total_profit / i), 2)
+        else:
+            print(f'Все фирмы работают в убыток')
+        average_profit = {'средняя прибыль': average_profit}
+        list_of_firms.extend([profit, average_profit])
+        print(f'{"-" * 30}\nПрибыль каждой компании - {list_of_firms}')
+    with open('text_7.json', 'w', encoding='utf-8') as my_file_js:
+        my_file_js.write(dumps(list_of_firms, sort_keys=False, indent=4, ensure_ascii=False, separators=(',', ': ')))
+        print(f'{"-" * 30}\nБыл создан результирующий файл "text_7.json"')
+except IOError:
+    print('Ошибка ввода-вывода!')
+finally:
+    my_f.close()
