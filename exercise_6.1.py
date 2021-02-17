f9cd78ed61978ab6498fa73e00b0d9f596a7e564
impreza555 <alex.erjomin@outlook.com>
2021-02-18 00:24:56 +0300

    Сделал упражнение 6.2, небольшие правки в упражнении 6.1

--- exercise_6.1.py
+++ exercise_6.1.py
@@ -2,13 +2,13 @@ from time import sleep
 
 
 class TrafficLight:
-    _color = [f'\033[31mКрасный\033[0m', f'\033[33mЖелтый\033[0m', f'\033[32mЗелёный\033[0m']
+    _color = [f'\033[31mКрасный', f'\033[33mЖелтый', f'\033[32mЗелёный']
 
     def running(self):
         i = 0
         for switch_time in [7, 2, 8]:
             color = self._color[i]
-            print(f'Включился {color} цвет!')
+            print(f'Включился {color}\033[0m цвет!')
             i += 1
             sleep(switch_time)
 
