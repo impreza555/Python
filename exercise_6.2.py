8aba3c7f44dd5f87439bcd92ca343d927be1ef7c
impreza555 <alex.erjomin@outlook.com>
2021-02-18 19:37:25 +0300

    Правки в упражнении 6.2.

--- exercise_6.2.py
+++ exercise_6.2.py
@@ -1,7 +1,10 @@
 class Road:
+    _length: float
+    _width: float
+
     def __init__(self, length, width):
-        self._length = float(length)
-        self._width = float(width)
+        Road._length = float(length)
+        Road._width = float(width)
 
     def calc_mass_asph(self, thickness):
         return round((self._length * self._width * 25 * float(thickness)) / 1000, 2)
@@ -9,8 +12,9 @@ class Road:
 
 length = input("Введите длинну: ")
 width = input("Введите ширину: ")
+thickness = input("Введите толщину: ")
 try:
     road = Road(length, width)
-    print(f'Масса асфальта равна {road.calc_mass_asph(input("Введите толщину: "))} тонн.')
+    print(f'Масса асфальта равна {road.calc_mass_asph(thickness)} тонн.')
 except ValueError:
     print('Ошибка ввода')
